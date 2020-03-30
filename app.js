b76e6c0aed7500423065b306739b52e4434b5cc7
Toth Zoltan <toth.zoltan.4@gmail.com>
2020-03-30 23:10:27 +0200

    Teszt

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ function MessageHandler() {
 
 MessageHandler.prototype.getSignature = function(m) {
     var crypto = require("crypto");
-    var replaceAll = function(x) {
-        return x.replace(/\s/g, '')
-    };
-    return crypto
+    var hmac = crypto
         .createHmac("sha384", "FxDa5w314kLlNseq2sKuVwaqZshZT5d6")
-        .update(m)
+        .update(m, "utf-8")
         .digest("base64");
+    return hmac;
 }
 
-
+/*
 var data = require("./data.json");
 var handler = new MessageHandler();
+console.log(JSON.stringify(data));
 console.log(handler.getSignature(JSON.stringify(data)));
+*/
